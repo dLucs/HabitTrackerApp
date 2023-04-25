0abbc68772b6691966723012f2b5693633f731ac
dLucs <Lucas@DESKTOP-2KAU8NT>
2023-04-25 15:44:13 +0200

    Added switch commands

--- HabitTrackerApp/Program.cs
+++ HabitTrackerApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Data.Sqlite;
+using Microsoft.VisualBasic.FileIO;
+using System.Data;
 
 namespace HabitTrackerApp
 {
@@ -18,6 +20,47 @@ namespace HabitTrackerApp
                 tableCmd.ExecuteNonQuery();
                 connection.Close();
             }
+        }
+        static void GetUserInput()
+        {
+            Console.Clear();
+            bool closeApp = false;
+            while (closeApp == false)
+            {
+                Console.WriteLine("\n\nMAIN MENU");
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("\nType 0 to Close Application.");
+                Console.WriteLine("Type 1 to View All Records.");
+                Console.WriteLine("Type 2 to Insert Record.");
+                Console.WriteLine("Type 3 to Delete Record.");
+                Console.WriteLine("Type 4 to Update Record.");
+                Console.WriteLine("-----------------------------------------\n");
+
+                string commandInput = Console.ReadLine();
+
+                switch (commandInput)
+                {
+                    case 0:
+                        Console.WriteLine("\nGoodBye!");
+                        closeApp = true;
+                        break;
+                    case 1:
+                        GetAllRecords();
+                        break;
+                    case 2:
+                        Insert();
+                        break;
+                    case 3:
+                        Delete();
+                        break;
+                    case 4:
+                        Update();
+                        break;
+                    default: Console.WriteLine("\nInvalid Command. Please Type a Number from 0 to 4.\n");
+                        break;
+                }
+            }
+
         }
     }
 }
